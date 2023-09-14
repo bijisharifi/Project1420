a1d369eb32e04f94e285b34a6876422058627deb
bijisharifi <123569908+bijisharifi@users.noreply.github.com>
2023-09-13 21:55:00 -0400

    Add files via upload

--- bst.py
+++ bst.py
@@ -59,25 +59,7 @@ def minNode(root: Node)-> Node:
 
 
 
-'''def remove(root: Node, key: int)-> Node:
-    if root is None:
-        return root
-    elif key < root.key:
-        root.leftchild = remove(root.leftchild, key)
-    elif key > root.key:
-        root.rightchild = remove(root.rightchild, key)
-    else:
-        if root.leftchild is None:
-            return root.rightchild
-        elif root.rightchild is None:
-            return root.leftchild
-        else:
-            successor = minNode(root.rightchild)
-            root.key = successor.key
-            root.keycount = successor.keycount
-            root.rightchild = remove(root.rightchild, root.key)
 
-        return root'''
 
 
 
@@ -92,20 +74,7 @@ def minNode(root: Node)-> Node:
 # If the key is not in the tree, do nothing.
 # If the key is in the tree, decrement its key count. If the keycount goes to 0, remove the key.
 # When replacement is necessary, use the inorder successor.
-'''def delete(root: Node, key: int) -> Node:
-    if root is None:
-        return root
 
-    if key < root.key:
-        root.leftchild = delete(root.leftchild, key)
-    elif key > root.key:
-        root.rightchild = delete(root.rightchild, key)
-    else:
-        if root.keycount > 1:
-            root.keycount -= 1
-        else:
-            root = remove(root, key)
-    return root'''
 
 def delete(root: Node, key: int) -> Node:
     if root is None:
@@ -139,6 +108,7 @@ def delete(root: Node, key: int) -> Node:
 # Calculate the list of keys on the path from the root towards the search key.
 # The key is not guaranteed to be in the tree.
 # Return the json.dumps of the list with indent=2.
+
 def search(root: Node, search_key: int) -> str:
     # YOUR CODE GOES HERE.
     # Then tweak the next line so it uses your list rather than None.
@@ -152,14 +122,20 @@ def search(root: Node, search_key: int) -> str:
         return json.dumps(loclist, indent=2)
     elif root.key < search_key:
         
-        res = search(root.leftchild, search_key)
-    else:
         res = search(root.rightchild, search_key)
+    else:
+        res = search(root.leftchild, search_key)
 
     if res:
-        loclist.extend(list(res))
+        loclist.append(res)
     
     return json.dumps(loclist, indent=2)
+
+
+'''root = Node(3, 1)
+root = insert(root, 2)
+print(search(root, 2))
+print("hello")'''
 
     
 
